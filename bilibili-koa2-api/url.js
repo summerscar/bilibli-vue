a31b7641eb68a9d9aaa17fbf991d90d12edda73e
summerscar <shen.yus@163.com>
2017-10-02 19:53:15 +0800

    day 2

---
+++ bilibili-koa2-api/url.js
@@ -0,0 +1,6 @@
+const imageUrl = {
+  header: 'http://i0.hdslb.com/bfs/archive/7194f83fcb011b609f354e27d7188c29586df444.png',
+  logo: 'http://i0.hdslb.com/bfs/archive/337c57bf55a5fa3fdad48a69ff3481d3dafaed99.png'
+}
+
+module.exports = {imageUrl}
